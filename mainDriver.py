84b23786cd095952ae12b64bc2c84a84b8de5dbf
Adam Spanier <aspanier@unomaha.edu>
2021-12-07 13:24:01 -0600

    Added Client ID LEdger

--- mainDriver.py
+++ mainDriver.py
@@ -7,18 +7,17 @@ import authenticate
 import clientKeyMining
 
 class Block:
-    def __init__(self, inputVal, concat, clientKey, next=None):
+    def __init__(self, inputVal, concat, next=None):
         self.inputVal = inputVal
         self.concat = concat
-        self.clientKey = clientKey
         self.next = next
         
 class Blockchain:
     def __init__(self):
         self.head = None
         
-    def insert(self, inputVal, concat, clientKey):
-        newNode = Block(inputVal, concat, clientKey)
+    def insert(self, inputVal, concat):
+        newNode = Block(inputVal, concat)
         if(self.head):
             current = self.head
             while(current.next):
@@ -28,31 +27,36 @@ class Blockchain:
             self.head = newNode
             
     def printBlockchain(self):
-        print("\n*****Printing Login Ledger*****")
+        #print("\n*****Printing Ledger*****")
         current = self.head
         while(current):
-            print("\n*****Login Record*****")
+            print("\n***** Record*****")
             print("Hashed Value: " + str(current.inputVal.hexdigest()))
             print("Value: " + str(current.concat))
-            print("Client Key: " + str(current.clientKey.hexdigest()))
             current = current.next
 
 def main():
     NUM_USERS = 5
     networkHash, authHash = getPassword()
     loginLedger = Blockchain()
+    clientIDLedger = Blockchain()
     for x in range(NUM_USERS):
-        concat = login(loginLedger, x, networkHash, authHash, NUM_USERS)
+        concat = login(clientIDLedger, loginLedger, x, networkHash, authHash, NUM_USERS)
+    
+    print("\n***** Printing Login Ledger *****")
     loginLedger.printBlockchain()
+    print("\n***** Printing Client ID Ledger *****")
+    clientIDLedger.printBlockchain()
         
-def login(loginLedger, x, networkHash, authHash, NUM_USERS):
+def login(clientIDLedger, loginLedger, x, networkHash, authHash, NUM_USERS):
     try:
         loginBlock, concat = authenticate.main(networkHash, authHash)
         loginBlock.hexdigest()
         print("\n*****Add Auth " + str(x) + " Login Ledger*****")
         acceptance = calcAcceptance(NUM_USERS)
         nonce, key = clientKeyMining.mineClientKey(concat, int(acceptance))
-        loginLedger.insert(loginBlock, concat, key)
+        loginLedger.insert(loginBlock, concat)
+        clientIDLedger.insert(key, concat)
         print("Login Record Hash: " + loginBlock.hexdigest())
         print("Client " + str((x+1)) + " Key: " + key.hexdigest())
         return concat
